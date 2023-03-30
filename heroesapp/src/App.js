9bc2cbc9fe6c453b8ea1042274c1efdcaf33b6f1
reeman9000 <preston.soper@student.awesomeinc.org>
2023-03-30 23:43:55 +0000

    not working api

--- heroesapp/src/App.js
+++ heroesapp/src/App.js
@@ -2,17 +2,26 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import HomePage from './HomePage';
 
+let url = 'https://8000-psoper1-djangopractice-qid21b643sn.ws-us93.gitpod.io/api/heroes/';
+// let username = 'admin';
+// let password = 'dataandyar';
+// let credentials = btoa(username + ':' + password);
+// let basicAuth = 'Basic ' + credentials;
+
+
 function App() {
-  const [data, setData] = useState({ data: [] });
-//   const [view, setView] = useState('Initial');
+  const [data, setData] = useState( [] );
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios(
-        'https://8000-psoper1-djangopractice-qid21b643sn.ws-us93.gitpod.io/api/heroes/',
-      );
-
+      // auth: {
+      //   username: uname,
+      //   password: pass
+      // }
+      // const result = await axios(url, { headers: {'Authorization': + basicAuth}});
+      const result = await axios(url);
       setData(result.data);
+      console.log(result.data[0].name)
     };
 
     fetchData();
--- heroesapp/src/HomePage.js
+++ heroesapp/src/HomePage.js
@@ -1,9 +1,13 @@
 function HomePage({data}) {
+
+  let hero_call = data.filter((hero) => hero.name.includes('Superman'))
+  console.log(hero_call.name)
+   
+
     return (
         <>
         <h1>Home page</h1>
-        <h1>Hero 1 is {data.data.filter((hero) => hero.name.includes('Superman')).map(item => (
-          <div key={item.name}></div>))}</h1>
+        <h1>Hero 1 is {hero_call.name}</h1>
         </>
     )
 }
