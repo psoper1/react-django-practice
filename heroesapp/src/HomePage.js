128c63c77b3feff497ab667a5f3a7872695c3461
reeman9000 <preston.soper@student.awesomeinc.org>
2023-03-31 18:14:07 +0000

    a

--- heroesapp/src/HomePage.js
+++ heroesapp/src/HomePage.js
@@ -1,15 +1,18 @@
-import {useState} from 'react';
+import { useState } from 'react';
+import { v4 } from 'uuid'
+
 
 function HomePage({ data, view, setView }) {
 
   let input = document.getElementById('input');
-  let hero_call = data.filter((hero) => hero.name.includes(input.value)).map((ree) => 
-  <>
-  <h1 key={ree.id} className="text-center">{ree.name}</h1>
-  <p key={ree.name} className="text-center">About Hero: {ree.about_me}</p>
-  <p className="text-center">Abilities:</p>
-  {ree.abilities.map((ree2) => <li className="text-center">{ree2.ability_name}</li>)}
-  </>)
+  let hero_call = data.filter((hero) => hero.name.includes(input.value)).map((ree) =>
+  
+    <>
+      <h1 key={ree.id} className="text-center">{ree.name}</h1>
+      <p key={ree.name} className="text-center">About Hero: {ree.about_me}</p>
+      <p className="text-center">Abilities:</p>
+      {ree.abilities.map((ree2) => <li key={v4()} className="text-center">{ree2.ability_name}</li>)}
+    </>)
   const [inputValue, setInputValue] = useState("");
 
   const handleUserInput = (e) => {
@@ -23,11 +26,11 @@ function HomePage({ data, view, setView }) {
 
   return (
     <>
-      <h1 className="text-center">Home page</h1>
       <div className="text-center">
-      <input id="input" placeholder="Type a hero name to search" value={inputValue} onChange={handleUserInput}/>
-      <button className="btn btn-primary" onClick={() => setView('Show all')}>Click to search</button>
-      <button className="btn btn-secondary" onClick={resetInputField}>Refresh Results</button>
+        <h1>Home page</h1>
+        <input id="input" placeholder="Type a hero name to search" value={inputValue} onChange={handleUserInput} />
+        <button className="btn btn-primary" onClick={() => setView('Show all')}>Click to search</button>
+        <button className="btn btn-secondary" onClick={resetInputField}>Refresh Results</button>
       </div>
       {view === 'Show all' && hero_call}
     </>
